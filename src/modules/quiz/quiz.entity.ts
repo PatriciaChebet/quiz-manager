ff4960609b393ae789d9ebf050b6fc10cb9f3601
Patricia <patriciacchebet@gmail.com>
2023-05-28 20:31:13 +0300

    Sets up TypeOrm and create Quiz Entity

---
+++ src/modules/quiz/quiz.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('quizes')
+export class Quiz extends BaseEntity{
+
+    @PrimaryGeneratedColumn({
+        comment: 'The quiz unique identifier',
+    })
+    id: number;
+
+    @Column({
+        type: 'varchar'
+    })
+    title: string;
+
+    @Column({
+        type: 'text'
+    })
+    description: string;
+
+    @Column({
+        type: 'boolean',
+        default: 1,
+    })
+    isActive: boolean;
+
+}
